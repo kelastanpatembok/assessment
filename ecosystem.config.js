8e48648e32dc89a04950c6da09cb561164056054
Eko S. Wibowo <swdev.bali@gmail.com>
2026-06-30 17:42:37 +0700

    show

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "assessment-LEGACY",
-      cwd: "/Users/eko/dev/SuperApp/assessment/LEGACY",
-      script: "/Users/eko/.nvm/versions/node/v20.20.2/bin/npm",
-      args: "run dev",
-      exec_mode: "fork",
-      env: { PORT: 2000 },
-    },
-    {
       name: "assessment-assessment-backend",
       cwd: "/Users/eko/dev/SuperApp/assessment/assessment/backend/",
       script: "mvn",
       args: "spring-boot:run",
       exec_mode: "fork",
-      env: { SERVER_PORT: 2001 },
+      env: { SERVER_PORT: 2002 },
       interpreter: "bash",
       watch: ["src/main/java/**/*.java", "src/main/resources/**/*.properties", "src/main/resources/**/*.yml"],
       watch_delay: 1500,
@@ -26,7 +18,7 @@ module.exports = {
       script: "/Users/eko/.nvm/versions/node/v20.20.2/bin/npm",
       args: "run dev",
       exec_mode: "fork",
-      env: { PORT: 2002 },
+      env: { PORT: 2000 },
     },
     {
       name: "assessment-auth-backend",
@@ -34,7 +26,7 @@ module.exports = {
       script: "mvn",
       args: "spring-boot:run",
       exec_mode: "fork",
-      env: { SERVER_PORT: 2003 },
+      env: { SERVER_PORT: 2001 },
       interpreter: "bash",
       watch: ["src/main/java/**/*.java", "src/main/resources/**/*.properties", "src/main/resources/**/*.yml"],
       watch_delay: 1500,
