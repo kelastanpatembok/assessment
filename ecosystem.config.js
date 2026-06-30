25009a490b0e0c3a049b7b754408d5a6ff99b321
Eko S. Wibowo <swdev.bali@gmail.com>
2026-06-30 21:08:08 +0700

    port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       name: "assessment-assessment-frontend",
       cwd: "/Users/eko/dev/SuperApp/assessment/assessment/frontend/",
       script: "/Users/eko/.nvm/versions/node/v20.20.2/bin/npm",
-      args: "run dev",
+      args: "run dev -- --host 0.0.0.0 --port 2000",
       exec_mode: "fork",
       env: { PORT: 2000 },
     },
